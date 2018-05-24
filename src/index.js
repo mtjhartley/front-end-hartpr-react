2a2e322cfeca61141818570237f9ac0873a11a31
mtjhartley <mtjhartley@gmail.com>
2018-05-24 00:04:42 -0700

    removed unnecessary comments from index.js also testing desktop git

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './stylesheets/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-//import ReactTable from "react-table";
-//import 'react-table/react-table.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
